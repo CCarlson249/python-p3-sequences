c7b0a1eb406fd9876ae4197eebbbd986bc467870
Clayton Carlson <claytonrc97@gmail.com>
2023-03-15 22:38:06 -0700

    lab complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    a, b = 0, 1
+    fib_sequence = []
+    for i in range(length):
+        fib_sequence.append(a)
+        a, b = b, a + b
+    print(fib_sequence)
     pass
